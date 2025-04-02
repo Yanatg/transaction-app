089448280974f9a09091d7c989ca2e73f7a334da
Yanatchara JERAJA <yanatchara.j@ku.th>
2025-04-02 10:47:39 +0700

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Path to your main HTML file
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Paths to all component files within src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
